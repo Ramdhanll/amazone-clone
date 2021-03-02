7fdaacc0915fb23b5c6f4c0aec0c8688b4528d57
ramdhanll <erdevlop@gmail.com>
2021-03-02 16:37:08 +0700

    add frontend

---
+++ frontend/src/data.js
@@ -0,0 +1,61 @@
+const data = {
+   products: [
+      {
+         _id: 1,
+         name: 'Nike Slim Shirt',
+         category: 'Shirts',
+         image: '/images/p1.jpg',
+         price: 120000,
+         brand: 'Nike',
+         rating: 4.5,
+         numReviews: 10,
+         description: 'high quality product'
+      },
+      {
+         _id: 2,
+         name: 'Nike Slim Shirt 2',
+         category: 'Shirts',
+         image: '/images/p1.jpg',
+         price: 120000,
+         brand: 'Nike',
+         rating: 4.5,
+         numReviews: 10,
+         description: 'high quality product'
+      },
+      {
+         _id: 3,
+         name: 'Nike Slim Shirt 3',
+         category: 'Shirts',
+         image: '/images/p1.jpg',
+         price: 120000,
+         brand: 'Nike',
+         rating: 4.5,
+         numReviews: 10,
+         description: 'high quality product'
+      },
+      {
+         _id: 4,
+         name: 'Nike Slim Shirt 4',
+         category: 'Shirts',
+         image: '/images/p1.jpg',
+         price: 120000,
+         brand: 'Nike',
+         rating: 4.5,
+         numReviews: 10,
+         description: 'high quality product'
+      },
+      {
+         _id: 5,
+         name: 'Nike Slim Shirt 5',
+         category: 'Shirts',
+         image: '/images/p1.jpg',
+         price: 120000,
+         brand: 'Nike',
+         rating: 4.5,
+         numReviews: 10,
+         description: 'high quality product'
+      },
+   ]
+}
+
+export default data
